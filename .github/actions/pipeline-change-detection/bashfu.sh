956b56a1871dfbb9d7ee49a14e559c2d9b2cd4fb
Oliver Luib <oliluib@gmail.com>
2021-11-26 15:19:07 +0100

    test

--- .github/actions/pipeline-change-detection/bashfu.sh
+++ .github/actions/pipeline-change-detection/bashfu.sh
@@ -1,3 +1,3 @@
-# DIRS_STRING=$1
-# git diff --name-only HEAD HEAD~1
-echo [not, the ,dirs, you_seek]
+[[ -z "$1" ]] && { echo "Parameter 1 is empty" ; exit 1; }
+DIR="$1"
+git diff --name-only HEAD HEAD~1 | awk -F/ '{print FS $1}' | grep -q -w $DIR && echo True || echo False
