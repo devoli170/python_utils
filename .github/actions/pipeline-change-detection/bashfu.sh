6a8bb18085d6e4be7894dcde5e11f7a779bf1f38
Oliver Luib <oliluib@gmail.com>
2021-11-26 22:34:27 +0100

    changing change detection

--- .github/actions/pipeline-change-detection/bashfu.sh
+++ .github/actions/pipeline-change-detection/bashfu.sh
@@ -1,3 +1,4 @@
 [[ -z "$1" ]] && { echo "Parameter 1 is empty" ; exit 1; }
 DIR="$1"
+echo ${GITHUB_REF##*/}
 git diff --name-only ${GITHUB_REF##*/}... | awk -F/ '{print FS $1}' | grep -q -w $DIR && echo true || echo false
