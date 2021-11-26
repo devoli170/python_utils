932e4585c8db154e878b24ad45433c78bae8cd4a
Oliver Luib <oliluib@gmail.com>
2021-11-26 11:58:39 +0100

    packaging, structure and test env setup works

--- 3D_windowing_edu/tests/test_windowing.py
+++ 3D_windowing_edu/tests/test_windowing.py
@@ -1,6 +1,8 @@
 import unittest
+from three_d_windowing_edu.windowing import glfw_window
+
 
 class TestAsciiMethods(unittest.TestCase):
 
     def test_(self):
-        self.assertTrue(True)
+        self.assertTrue(glfw_window.tdd_true())
